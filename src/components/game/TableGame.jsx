bcde09195bf4fb2012ac1efed02001dc4c855ab8
Juan Palo <spokendrack_123@hotmail.com>
2021-05-24 17:39:40 -0500

    Canbios

--- src/components/game/TableGame.jsx
+++ src/components/game/TableGame.jsx
@@ -3,16 +3,17 @@ import { Square } from './Square'
 
 export const TableGame = () => {
     return (
-        <div className=" w-3/4 " >
-            <Square clase=" w-1/3 bg-red-600" />
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
+        <div className="grid grid-cols-3 w-1/3  h-4/6 ">
+            <Square clase=" w-full h-full bg-red-600 borde border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-yellow-400 border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-green-500 border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-blue-300 border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-indigo-700 border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-pink-500 border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-black border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-yellow-600 border-solid border-2 border-black" />
+            <Square clase=" w-full h-full bg-yellow-900 border-solid border-2 border-black" />
+            
             
         </div>
     )
