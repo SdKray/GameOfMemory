438c5ac18b4a45df37af6a64215deeb519cb76ab
Luis Alberto Baca Cortes <luis.98bc@gmail.com>
2022-02-14 20:25:54 -0600

    indented code

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export const LoginScreen = () => {
     return (
-        <div className=' flex flex-col justify-center items-center w-full h-full bg-gray-700'>
+        <div className='flex flex-col justify-center items-center w-full h-full bg-gray-700'>
             <div className='flex flex-col  w-3/12 h-3/5 mb-12 p-4  justify-around rounded-lg shadow-lg bg-white'>
                 <h1 className='font-serif text-4xl font-black'>LOGIN</h1>
                 <label className=' font-serif text-2xl' htmlFor='Score'>
                     Email:
                 </label>
-                <input className='h-8 rounded-lg bg-gray-300 pl-3' type='text'/>
+                <input className='h-8 rounded-lg bg-gray-300 pl-3' type='text' autoComplete='off'/>
                 <label className='font-serif text-2xl' htmlFor=''>
                     Pasword:
                 </label>
-                <input type='password' className='h-8 rounded-lg  bg-gray-300 pl-4' />
+                <input type='password' className='h-8 rounded-lg  bg-gray-300 pl-4' autoComplete='off'  />
                 <a className=' text-blue-500 text-right' href='/'>
                     Forget Pasword?
                 </a>
--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,69 +1,40 @@
 import React from 'react';
 
 export const RegisterScreen = () => {
-	return (
-		<div className='flex items-center w-full h-full bg-gray-500'>
-			<form
-				action=''
-				className='flex flex-col w-96 p-4 rounded-lg shadow-lg mx-auto bg-gray-50'>
-				<h1 className='text-3xl font-bold mb-4'>Register</h1>
-				<div className='flex flex-row'>
-					<div className='flex flex-col'>
-						<label htmlFor='' className='text-2xl mb-2 font-medium'>
-							Name:
-						</label>
-						<input
-							type='text'
-							name=''
-							id=''
-							className='font-medium h-8 px-2 w-11/12 bg-gray-300 rounded-lg'
-						/>
-					</div>
-					<div className='flex flex-col'>
-						<label htmlFor='' className='text-2xl mb-2 font-medium'>
-							Last Name:
-						</label>
-						<input
-							type='text'
-							name=''
-							id=''
-							className='font-medium h-8 px-2 w-full bg-gray-300 rounded-lg'
-						/>
-					</div>
-				</div>
-				<label htmlFor='' className='text-2xl font-medium mb-2 mt-2'>
-					Email:
-				</label>
-				<input
-					type='email'
-					name=''
-					id=''
-					className='font-medium px-2 h-8 bg-gray-300 rounded-lg'
-				/>
-				<label htmlFor='' className='text-2xl font-medium mb-2 mt-2'>
-					Password:
-				</label>
-				<input
-					type='password'
-					name=''
-					id=''
-					className='font-medium px-2 h-8 bg-gray-300 rounded-lg'
-				/>
-				<label htmlFor='' className='text-2xl font-medium mb-2 mt-2'>
-					Repeat Password:
-				</label>
-				<input
-					type='password'
-					name=''
-					id=''
-					className='font-medium px-2 h-8 bg-gray-300 rounded-lg'
-				/>
-				<button
-					type='submit'
-					className='text-white text-2xl my-7 py-1 bg-blue-600  rounded-lg'>
-					Register Now
-				</button>
-			</form>
-		</div>
-	);
+    return (
+        <div className='flex items-center w-full h-full bg-gray-500'>
+            <form action='' className='flex flex-col w-96 p-4 rounded-lg shadow-lg mx-auto bg-gray-50'>
+                <h1 className='text-3xl font-bold mb-4'>Register</h1>
+                <div className='flex flex-row'>
+                    <div className='flex flex-col'>
+                        <label htmlFor='' className='text-2xl mb-2 font-medium'>
+                            Name:
+                        </label>
+                        <input type='text' name='' id='' className='font-medium h-8 px-2 w-11/12 bg-gray-300 rounded-lg' />
+                    </div>
+                    <div className='flex flex-col'>
+                        <label htmlFor='' className='text-2xl mb-2 font-medium'>
+                            Last Name:
+                        </label>
+                        <input type='text' name='' id='' className='font-medium h-8 px-2 w-full bg-gray-300 rounded-lg' />
+                    </div>
+                </div>
+                <label htmlFor='' className='text-2xl font-medium mb-2 mt-2'>
+                    Email:
+                </label>
+                <input type='email' name='' id='' className='font-medium px-2 h-8 bg-gray-300 rounded-lg' />
+                <label htmlFor='' className='text-2xl font-medium mb-2 mt-2'>
+                    Password:
+                </label>
+                <input type='password' name='' id='' className='font-medium px-2 h-8 bg-gray-300 rounded-lg' />
+                <label htmlFor='' className='text-2xl font-medium mb-2 mt-2'>
+                    Repeat Password:
+                </label>
+                <input type='password' name='' id='' className='font-medium px-2 h-8 bg-gray-300 rounded-lg' />
+                <button type='submit' className='text-white text-2xl my-7 py-1 bg-blue-600  rounded-lg'>
+                    Register Now
+                </button>
+            </form>
+        </div>
+    );
 };
