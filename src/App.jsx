e29632c288b4edeb96ba0618a6657804cc7bfc47
Luis Alberto Baca Cortes <luis.98bc@gmail.com>
2022-02-14 20:17:40 -0600

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
+import React from 'react';
+import { LoginScreen } from './components/auth/LoginScreen';
+import { GameScreen } from './components/game/GameScreen';
+import { Navbar } from './components/ui/Navbar';
+import { ScoreResult } from './components/game/ScoreResult';
+import { RegisterScreen } from './components/auth/RegisterScreen';
 
-import React from 'react'
-import { LoginScreen } from './components/auth/LoginScreen'
-import { GameScreen } from './components/game/GameScreen'
-import { Navbar } from './components/ui/Navbar'
-import {ScoreResult} from './components/game/ScoreResult'
-import {RegisterScreen} from './components/auth/RegisterScreen'
-export const App = ({name}) => {
+export const App = ({ name }) => {
     return (
         <>
-            
-            <ScoreResult/>
-            
+            <LoginScreen />
         </>
-    )
-}
+    );
+};
